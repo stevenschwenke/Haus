91f450c45a3932ea2e22e457b627add97c0488fd
Steven Schwenke <steven@stevenschwenke.de>
2014-05-31 19:16:06 +0200

    Hello World.

---
+++ src/main/java/Start.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+
+
+        Scene scene = new Scene(root, 400, 200);
+
+        stage.setTitle("Haus");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/StartController.java
@@ -0,0 +1,31 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.util.Duration;
+
+public class StartController {
+
+    @FXML
+    private AnchorPane rootAnchorPane;
+
+    @FXML
+    void initialize() {
+
+    }
+}
