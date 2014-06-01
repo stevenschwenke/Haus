f8bd7721913acec997203ea656131b96eed76aa4
Steven Schwenke <steven@stevenschwenke.de>
2014-06-01 10:20:58 +0200

    added time-label and styling

--- src/main/java/StartController.java
+++ src/main/java/StartController.java
@@ -1,20 +1,40 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class StartController {
 
     @FXML
-    Button closeButton;
-
-    @FXML
-    private AnchorPane rootAnchorPane;
+    private Label date;
 
     @FXML
     void initialize() {
+        startClock();
+    }
 
+    private void startClock() {
+
+        final DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss, dd.MM.yyyy");
+
+        final Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // TODO This is kind of ugly ... is a new calendar instance created every second?
+                Calendar cal = Calendar.getInstance();
+                date.textProperty().set(dateFormat.format(cal.getTime()));
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.playFromStart();
     }
 
     @FXML
